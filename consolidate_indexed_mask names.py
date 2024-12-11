0c686cde7a790ea37909219a59b3784baab3d1bf
Dmytro Holub <heap3d@gmail.com>
2024-12-11 10:23:12 +0200

    initial commit for consolidate_indexed_mask_names.py

---
+++ consolidate_indexed_mask names.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+# ================================
+# (C)2024 Dmytro Holub
+# heap3d@gmail.com
+# --------------------------------
+# modo python
+# consolidate materials with indexed names
+# ================================
