2d0efd8351a92e9d7618ce78675f8609173a8890
Dmytro Holub <heap3d@gmail.com>
2024-12-11 13:34:24 +0200

    in progress: consolidate_indexed_mask_names

--- consolidate_indexed_mask names.py
+++ consolidate_indexed_mask names.py
@@ -6,3 +6,21 @@
 # modo python
 # consolidate materials with indexed names
 # ================================
+
+import modo
+
+
+def main():
+    ...
+
+
+def get_tag_masks_dict() -> dict[str, modo.Item]:
+    ...
+
+
+def get_tags() -> list[str]:
+    ...
+
+
+if __name__ == '__main__':
+    main()
